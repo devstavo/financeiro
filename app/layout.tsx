345447b72456499a49d9d758eac1bca252d2dd97
Gustavo antonio Farinea <87901782+devstavo@users.noreply.github.com>
2025-07-17 14:13:46 +0000

    feat: Adiciona funcionalidade XYZ

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Financeiro Farinea',
+  description: 'Financeiro Farinea - Controle Financeiro',
+  generator: 'Financeiro Farinea',
 }
 
 export default function RootLayout({
